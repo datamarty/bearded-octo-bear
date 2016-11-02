f59f3459c22b38eca7b7721265a44c748bdc0237
Martin Selway <datamarty@users.noreply.github.com>
2016-11-02 17:18:05 +0000

    V1 of completed function

--- best.R
+++ best.R
@@ -9,26 +9,50 @@ best <- function(state, outcome) {
         result <- read.csv('outcome-of-care-measures.csv', colClasses = "character")
         ## Check that state and outcome are valid
         states <- unique(result$State) # unique states available
-        # state <- "TX" # Debug test value
+        outcomes <- c("heart attack", "heart failure", "pneumonia")
+         #state <- "TX" # Debug test value
+         #outcome <- "pneumonia" # Debug test value
         valid.state <-match(state, states) # > 0 if state exists
-        
+        valid.outcome <- match(outcome, outcomes) # > 0 if outcome valid
+        if (valid.state < 1) stop ("state passed as argument is not valid")
+        if (valid.outcome < 1) stop ("outcome passed as argument is not valid")
         ## Return hospital name in that state with lowest 30-day death rate
+        if (outcome == "heart attack") {
+                output <- result[, c(2, 7, 11)] #Hospital name, State, outcome
+        }
+        if (outcome == "heart failure") {
+                output <- result[, c(2, 7, 17)] #Hospital name, State, outcome
+        }
+        if (outcome == "pneumonia") {
+                output <- result[, c(2, 7, 23)] #Hospital name, State, outcome
+        }
         
+        output <- output[, c(1,3)] # discard State column, no longer needed
+        r <- range(as.numeric(output[,2]), na.rm = TRUE) # get the min and max mortality rates
+        # the minimum rate corresponds to the best outcome, get the Hospital name from the data frame
+        r <- min(r) # use this value to match on to get the 'Best' hospital
+        dfnames <- c("Hospital.Name", "outcome")
+        names(output) <- dfnames # rename data frame columns
+        output <- subset(output, outcome == r, select = 1:2) # limit output to Hospitals with outcome == minimum range
+        
+        output <- sort(output[1]) # only return first sorted name
+        return(output) # return the output
 }
 
 
 ## Here is some sample output from the function.
-#> source("best.R")
-#> best("TX", "heart attack")
+#source("best.R")
+#best("TX", "heart attack")
 #[1] "CYPRESS FAIRBANKS MEDICAL CENTER"
-#> best("TX", "heart failure")
+#best("TX", "heart failure")
 #[1] "FORT DUNCAN MEDICAL CENTER"
 #> best("MD", "heart attack")
 #[1] "JOHNS HOPKINS HOSPITAL, THE"
-#> best("MD", "pneumonia")
-#[1] "GREATER BALTIMORE MEDICAL CENTER"
+#> suppressWarnings(best("TX", "pneumonia"))
+#[1] CEDARS-SINAI MEDICAL CENTER
 #> best("BB", "heart attack")
 #Error in best("BB", "heart attack") : invalid state
-#> best("NY", "hert attack")
-#Error
+#> suppressWarnings(best("PA", "eart failur"))
+#Error in if (valid.outcome < 1) stop("outcome passed as argument is not valid") : 
+#missing value where TRUE/FALSE needed
 ##
